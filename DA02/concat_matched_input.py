c8eff1e3a7703d15d88db49a8066e88ef3cab9d1
AlbertoRosado1 <ar652820@ohio.edu>
2022-07-01 09:02:01 -0700

    Moved runlists to seperate folder. Did final checks for south, and north again.

--- DA02/concat_matched_input.py
+++ DA02/concat_matched_input.py
@@ -1,12 +1,21 @@
 import os
-# to concatenate matched inputs 00 to 14 for south
+import sys
+
+run = str(sys.argv[1]) #'south'
+nber_files = int(sys.argv[2]) # 30
+
+# to concatenate matched inputs 00 to 29 for south
 from astropy.table import Table, vstack
-run = 'south'
 merged_dir = os.path.join(os.getenv('CSCRATCH'),'legacysim','dr9','DA02',run,'file0_rs0_skip0','merged')
-nber_files = 12
-file_idx = file_idx = ["%.2d"%i for i in range(nber_files)]
+print(f"reading from {merged_dir}")
+file_idx = ["%.2d"%i for i in range(nber_files)]
+print(f"File indexes: {file_idx}")
 #Read in the fits table you want to append 
 # Use Astropy's 'vstack' function
+print(f"Start concatenating files")
 concat_legacysim = vstack([Table.read(os.path.join(merged_dir,f'matched_inputs_{i}.fits'), format='fits') for i in file_idx])
+print(f"Finished concatenating files")
 # Create fits file with new concatenated randoms
+print(f"saving {os.path.join(merged_dir,'matched_input_concatenated.fits')}")
 concat_legacysim.write(os.path.join(merged_dir,'matched_input_concatenated.fits'), format='fits', overwrite=False)
+print(f"saved {os.path.join(merged_dir,'matched_input_concatenated.fits')}")
